72d05f3fe25c768295156aa950430c871c67abbe
Emily Deng <emrdeng21@gmail.com>
2023-03-29 19:16:25 -0400

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const MongoStore = require('connect-mongo');
+const cors = require ("cors");
+
+var authHandler = require('./routes/auth.js');
+var routeHandler = require('./routes/handler.js');
+require("./models/passport");
+
+let app = express();
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: "GET, POST, PUT, DELETE",
+  credentials: true,
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      // maxAge: new Date(Date.now() + (1000*60*60*24))
+      maxAge: 1000*60*60*24
+    },
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URL,
+      collectionName: "sessions",
+    })
+  }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req,res,next)=>{
+  // console.log(req.session);
+  // console.log(req.user);
+  next();
+});
+
+app.use("/auth", authHandler);
+app.use("/", routeHandler);
+
+app.listen(process.env.PORT,function(req,res){
+    console.log(`Server is running on Port ${process.env.PORT}.`)
+  });
